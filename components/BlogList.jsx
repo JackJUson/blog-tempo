c156caf2b4daaf75482ea54e20e3ed8c132710d2
Jackson Jung <jackson.w.jung@gmail.com>
2024-07-15 21:29:53 +1000

    Add admin components and update bloglist

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,11 +1,23 @@
 'use client';
 
 import { blog_data } from '@/assets/assets';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BlogItem from './BlogItem';
+import axios from 'axios';
 
 const BlogList = () => {
   const [menu, setMenu] = useState('All');
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    const response = await axios.get('/api/blog');
+    setBlogs(response.data);
+    console.log('Blogs: ', response.data);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
 
   return (
     <div>
@@ -36,13 +48,13 @@ const BlogList = () => {
         </button>
       </div>
       <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
-        {blog_data
+        {blogs
           .filter((blog) => (menu === 'All' ? true : blog.category === menu))
           .map((blog, index) => {
             return (
               <BlogItem
                 key={index}
-                id={blog.id}
+                id={blog._id}
                 image={blog.image}
                 title={blog.title}
                 description={blog.description}
